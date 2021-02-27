dd439676df1b5c6863455f8ec91e1b388666c8e0
mopemope-LeaF <ulaqingdan@gmail.com>
2021-02-27 15:55:50 +0800

    why

--- lib.rs
+++ lib.rs
@@ -12,7 +12,9 @@ mod vote_manager {
     use ink_storage::{
         collections::{
             HashMap as StorageHashMap,
+            Vec as StorageVec,
         },
+        Box as StorageBox,
         traits::{
             PackedLayout,
             SpreadLayout,
@@ -20,6 +22,7 @@ mod vote_manager {
     };
 
     type VoteId = u64;
+    type ChoiceId = u32;
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode, PackedLayout, SpreadLayout)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink_storage::traits::StorageLayout))]
@@ -35,6 +38,24 @@ mod vote_manager {
         derive(
             Debug,
             PartialEq,
+            Clone,
+            Eq,
+            scale_info::TypeInfo,
+            ink_storage::traits::StorageLayout
+        )
+    )]
+    pub struct Choice {
+        choice_id: ChoiceId,
+        content: String,
+        yea: u64,
+    }
+
+    #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(
+        feature = "std",
+        derive(
+            Debug,
+            PartialEq,
             Eq,
             scale_info::TypeInfo,
             ink_storage::traits::StorageLayout
@@ -42,21 +63,21 @@ mod vote_manager {
     )]
     pub struct Vote {
         executed: bool,
+        title: String,
+        desc: String,
         start_date: u64,
         vote_time: u64,
         support_require_pct: u64,
-        yea: u64,
-        nay: u64,
+        min_require_num: u64,
+        support_num: u64,
+        choices: StorageBox<StorageVec<Choice>>,
     }
 
     #[ink(storage)]
     pub struct VoteManager {
-        support_require_pct: u64,
-        min_require_num: u64,
         votes_length: u64,
-        vote_time: u64,
         votes: StorageHashMap<VoteId, Vote>,
-        voters: StorageHashMap<(VoteId, AccountId), VoterState>,
+        voters: StorageHashMap<(VoteId, AccountId), ChoiceId>,
     }
 
     #[ink(event)]
@@ -76,7 +97,7 @@ mod vote_manager {
         #[ink(topic)]
         voter: AccountId,
 
-        support: bool,
+        support_choice: ChoiceId,
     }
 
     #[ink(event)]
@@ -88,70 +109,78 @@ mod vote_manager {
     impl VoteManager {
 
         #[ink(constructor)]
-        pub fn new(_vote_time: u64, support_require_pct: u64, min_require_num: u64) -> Self {
+        pub fn new() -> Self {
             Self { 
-                support_require_pct,
-                min_require_num,
                 votes_length: 0,
-                vote_time: _vote_time,
                 votes: StorageHashMap::default(),
                 voters: StorageHashMap::default(),
             }
         }
 
         #[ink(message)]
-        pub fn new_vote(&mut self) {
+        pub fn new_vote(&mut self, title: String, desc: String, vote_time: u64, support_require_pct: u64, min_require_num: u64, choices: String) -> u64 {
             let vote_id = self.votes_length.clone();
             self.votes_length += 1;
             let start_date: u64 = self.env().block_timestamp();
-            let vote = Vote{
+            let mut vote = Vote{
                 executed: false,
+                title,
+                desc,
                 start_date: start_date,
-                vote_time: self.vote_time.clone(),
-                support_require_pct: self.support_require_pct.clone(),
-                yea: 0,
-                nay: 0,
+                vote_time,
+                support_require_pct,
+                min_require_num,
+                support_num: 0,
+                choices: StorageBox::new(StorageVec::default()),
             };
+            let mut index = 0;
+            let split = choices.split(",");
+            for choice_content in split {
+                vote.choices.push(Choice{
+                    choice_id: index,
+                    content: choice_content.to_string(),
+                    yea: 0,
+                });
+                index += 1;
+            }
             self.votes.insert(vote_id, vote);
             self.env().emit_event(StartVote{
                 vote_id,
                 creator: self.env().caller(),
             });
+            vote_id
         }
 
         #[ink(message)]
-        pub fn vote(&mut self, vote_id: VoteId,  support: bool, voter: AccountId) {
-            // assert!(self.vote_exists(vote_id));
-            if let Some(_vote) = self.votes.get_mut(&vote_id) {
-                if let Some(vote_state) = self.voters.get(&(vote_id, voter)) {
-                    match vote_state {
-                        VoterState::Yea => {
-                            _vote.yea -= 1;
-                        },
-                        VoterState::Nay => {
-                            _vote.nay -= 1;
-                        },
-                        VoterState::Absent => (),
+        pub fn vote(&mut self, vote_id: VoteId, support_choice: u32, voter: AccountId) -> bool {
+            if !self.vote_exists(vote_id) {
+                return false;
+            }
+            if let Some(vote) = self.votes.get_mut(&vote_id) {
+                if support_choice > vote.choices.len() {
+                    return false;
+                }
+                let choices = &mut vote.choices;
+                let voter_choice = choices.get_mut(support_choice).unwrap();
+                // has voted
+                if let Some(choice_id) = self.voters.get(&(vote_id, voter)) {
+                    if *choice_id != support_choice {
+                        choices.get_mut(*choice_id).unwrap().yea -= 1;
+                        vote.support_num -= 1;
                     }
-                }
-                if support {
-                    _vote.yea += 1;
-                    self.voters.insert((vote_id, voter), VoterState::Yea);
-                } else {
-                    _vote.nay += 1;
-                    self.voters.insert((vote_id, voter), VoterState::Nay);
-                }
+                } 
+                voter_choice.yea += 1;
+                self.voters.insert((vote_id, voter), support_choice);    
+                vote.support_num += 1;
                 self.env().emit_event(CastVote{
                     vote_id,
                     voter: self.env().caller(), 
-                    support,
+                    support_choice,
                 });
+                true
+            } else {
+                false
             }
-        }
-
-        #[ink(message)]
-        pub fn next_index(&self) -> u64 {
-            self.votes_length
         }
 
         fn vote_exists(&self, vote_id: u64) -> bool {
@@ -159,7 +188,15 @@ mod vote_manager {
         }
 
         fn is_vote_open(&self, vote: Vote) -> bool {
-            return self.env().block_timestamp() < vote.start_date + self.vote_time && !vote.executed;
+            return self.env().block_timestamp() < vote.start_date + vote.vote_time && !vote.executed;
+        }
+
+        fn is_vote_executed(&self, vote: Vote) -> bool {
+            return !vote.executed;
+        }
+
+        fn is_vote_finished(&self, vote: Vote) -> bool {
+            return self.env().block_timestamp() < vote.start_date + vote.vote_time;
         }
     }
 }
